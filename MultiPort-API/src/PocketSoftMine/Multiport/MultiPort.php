2af528c4d475ab4e2b3481da3b6cc32cda403a64
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-04-03 18:44:55 -0300

    Update MultiPort.php

--- MultiPort-API/src/PocketSoftMine/Multiport/MultiPort.php
+++ MultiPort-API/src/PocketSoftMine/Multiport/MultiPort.php
@@ -2,6 +2,7 @@
 
 namespace PocketSoftMine\Multiport\MultiPort;
 
+use Multiport\task\LoggerTask;
 use pocketmine\network\protocol\Info;
 use pocketmine\plugin\PluginBase;
 
@@ -10,12 +11,18 @@ class MultiPort extends PluginBase{
 	const TARGET_PROTOCOL = 45;
 
 	const CURRENT_MINECRAFT_VERSION_NETWORK = "0.14.0";
+	
+	private $port;
+	private $upnp;
+	
+	private $getIP;
+	private $loaderPort;
 
 	public function onEnable(){
 		$this->saveDefaultConfig();
 	}
 	
 	public function loadPort($port, $upnp){
-		
+		$port = new LoggerTask($this->getPlugin(), [], ($sender instanceof Player ? $sender : false));
 	}
 }
