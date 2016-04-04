f60692478f08a90d12e942805419917581e4e102
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-04-04 15:42:06 -0300

    Update MultiPort.php

--- MultiPort-API/src/PocketSoftMine/Multiport/MultiPort.php
+++ MultiPort-API/src/PocketSoftMine/Multiport/MultiPort.php
@@ -3,7 +3,23 @@
 namespace PocketSoftMine\Multiport\MultiPort;
 
 use Multiport\task\LoggerTask;
-use pocketmine\network\protocol\Info;
+
+use pocketmine\network\AdvancedSourceInterface;
+use pocketmine\event\player\PlayerCreationEvent;
+use pocketmine\network\protocol\BatchPacket;
+use pocketmine\network\protocol\DataPacket;
+use pocketmine\network\protocol\Info as ProtocolInfo;
+use pocketmine\utils\Binary;
+use pocketmine\utils\MainLogger;
+use pocketmine\Player;
+use pocketmine\Server;
+use raklib\protocol\EncapsulatedPacket;
+use raklib\RakLib;
+use raklib\server\RakLibServer;
+use raklib\server\ServerHandler;
+use raklib\server\ServerInstance;
+use pocketmine\network\Network;
+use pocketmine\network\CachedEncapsulatedPacket;
 use pocketmine\plugin\PluginBase;
 
 class MultiPort extends PluginBase{
@@ -11,7 +27,28 @@ class MultiPort extends PluginBase{
 	const TARGET_PROTOCOL = 45;
 
 	const CURRENT_MINECRAFT_VERSION_NETWORK = "0.14.0";
-	const GET_PORT = [19132, 19133, 19134, ...]
+	const GET_PORT = "19132";
+	
+	/** @var Server */
+	private $server;
+
+	/** @var Network */
+	private $network;
+
+	/** @var RakLibServer */
+	private $rakLib;
+
+	/** @var Player[] */
+	private $players = [];
+
+	/** @var string[] */
+	private $identifiers;
+
+	/** @var int[] */
+	private $identifiersACK = [];
+
+	/** @var ServerHandler */
+	private $interface;
 	
 	private $port;
 	private $upnp;
@@ -25,5 +62,33 @@ class MultiPort extends PluginBase{
 	
 	public function loadPort($port, $upnp){
 		$port = new LoggerTask($this->getPlugin(), [], ($this instanceof Player ? $this : false));
+	}
+	
+	public function __construct(Server $server, $port = 19133){
+
+		$this->server = $server;
+		$this->identifiers = [];
+
+		$this->rakLib = new RakLibServer($this->server->getLogger(), $this->server->getLoader(), $port, $this->server->getIp() === "" ? "0.0.0.0" : $this->server->getIp());
+		$this->interface = new ServerHandler($this->rakLib, $this);
+	}
+	
+	public function setNetwork(Network $network){
+		$this->network = $network;
+	}
+	
+	public function process(){
+		$work = false;
+		if($this->interface->handlePacket()){
+			$work = true;
+			while($this->interface->handlePacket()){
+		}
+	} if($this->rakLib->isTerminated()){
+			$this->network->unregisterInterface($this);
+
+			throw new \Exception("RakLib Thread crashed");
+		}
+
+		return $work;
 	}
 }
