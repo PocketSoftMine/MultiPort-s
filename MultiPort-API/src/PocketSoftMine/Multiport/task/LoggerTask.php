e65027261e3dc752b778d3855afe3c166ba3fddc
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-04-06 13:55:07 -0300

    Update LoggerTask.php

--- MultiPort-API/src/PocketSoftMine/Multiport/task/LoggerTask.php
+++ MultiPort-API/src/PocketSoftMine/Multiport/task/LoggerTask.php
@@ -5,10 +5,26 @@ namespace LoggerTask\task\Multiport;
 use pocketmine\Server;
 use pocketmine\scheduler\PluginTask;
 use pocketmine\Player;
+use pocketmine\event;
+use pocketmine\PocketMine;
 
 class LoggerTask{
   public function onEnable(){
+    
     private $loggertask;
+    
+    if(!$this->log($loggertask)->getLoggerTask($task, $name, $player, $send, $loggertask) == null){
+      
+      return null;
+      
+      $task = new loggertask($this->getPlugin()->getLoggerTask() == false);
+      if(!$task == true){
+      
+        return true;
+      
+        break;
+      }
+    }
   }
   
 }
